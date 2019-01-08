e45832bc2a04b4a6bce0beebb6c504ee45e67197
wadeling <wadeling@gmail.com>
2019-01-08 16:32:31 +0800

    Update README.go
    modify image path

--- cgroup/README.go
+++ cgroup/README.go
@@ -1,11 +1,13 @@
-## result
+## result 
+
 ![result](https://github.com/wadeling/dockerpractice/blob/master/cgroup/images/result.png)
+          
 golang 执行 cmd.Start 相当于fork了一个子进程
 
 ## pstree
-![pstree](https://github.com/wadeling/dockerpractice/raw/master/cgroup/images/pstre.png)
+![pstree](https://github.com/wadeling/dockerpractice/blob/master/cgroup/images/pstre.png)
 stress 有两个进程，一个是strees本身进程，另外一个是strees -m 1参数生成的子进程
 
 ## tasks 
-![tasks](https://github.com/wadeling/dockerpractice/raw/master/cgroup/images/tasks.png)
+![tasks](https://github.com/wadeling/dockerpractice/blob/master/cgroup/images/tasks.png)
 tasks里面有很多进程id，程序里面只加了一个id，子进程id是自动加入的
